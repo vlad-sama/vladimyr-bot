94a0bd1bcfa333cb762bc3ab47b652e501cfae25
Pelpav <112887005+Pelpav@users.noreply.github.com>
2022-09-14 02:26:28 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,128 @@
+<p align="center">
+   <a href="https://github.com/Pelpav">
+    <img src="https://giffiles.alphacoders.com/345/34556.gif" alt="animated" width="540" height="280" />
+     
+</p>
+<h1 align="center"> lelouch (Bêta)
+</h1>
+<p align="center"> 
+  Hey, je suis lelouch... 
+Je voulais créer un bot en français car je n'en ai pas encore vu. Je remercie Pelpav pour son travail remarquable.
+  <p align="center">
+          je fais des changements ou des ajouts habituel parfois pour rendre le bot plus fun. je noterai la date des derniers reglages pour ceux qui veulent en  profiter aussi.
+ <p align="center">                                              
+<p align="center">
+  <a href="https://github.com/Pelpav/Lelouch">
+    <img src="https://img.shields.io/github/forks/Pelpav/Lelouch?label=Fork&style=social">
+
+
+<p align="center">
+  <a href="https://github.com/Pelpav/Lelouch">
+    <img src="https://visitor-badge.glitch.me/badge?page_id=https://github.com/Pelpav/Lelouch.visitor-badge&left_text=Total%20Repo%20Visits">
+    
+</br>
+      
+</br> 
+   
+   #### ✧✧ Ce bot est toujours en cours de développement, donc si vous souhaitez le recoder/le modifier, veuillez vérifier ce référentiel principal une fois tous les 2 jours car je le débogue en permanence et y apporte des modifications majeures.
+</br>
+  
+## ✧✧✧ Méthode de déploiement Heroku :
+
+#### ✧ `Fork` ce dépôt.
+
+#### ✧ Cliquez sur ce bouton de numérisation ci-dessous et numérisez le QR donné dans les 15 secondes depuis votre WhatsApp.
+
+<a href="https://Lelouch-qr--Pelpav.repl.co/"><img src="https://play-lh.googleusercontent.com/901aMQFFnVoX2T-YuJmTIwpPve_SUgMv_QSyzMSPtAqt_l0CyXN1DxfD6xXU0r2f9iM=w240-h480-rw" largeur ="90" />
+</a>
+
+#### Ou, Scannez indirectement depuis mon `repl.it`
+
+#### <a href="https://replit.com/@Pelpav/Lelouch?v=1"><img src="https://i.ibb.co/pPQjJL2/replit-logo- png-transparent.png" alt="replit-logo-png-transparent" border="0" width="120"></a>
+
+#### ✧ Téléchargez le fichier `session.json` et téléchargez-le sur la page d'accueil de votre `Fork`.
+
+#### ✧ Accédez à votre compte `Heroku`.
+
+#### ✧ Créez une "application aléatoire" dans Heroku avec n'importe quel nom.
+
+#### ✧ Connectez votre `GitHub` avec `Heroku`
+
+#### ✧ Connectez ensuite `Lelouch's repo` depuis votre GitHub.
+
+#### ✧ Allez dans les `settings` de Heroku ---> `Add Buildpacks` puis connectez un à un ces 3 buildpacks mentionnés ci-dessous.
+
+#### ✧ Revenez ensuite à `Heroku` ---> `Deploy` puis déployez l'application
+
+#### ✧ Une fois le déploiement terminé, accédez à `Heroku` ---> `Ressources` et activez le commutateur.
+
+#### ✧ Profitez :)
+
+## ✧ Heroku Builbpacks:
+
+```
+https://github.com/jonathanong/heroku-buildpack-ffmpeg-latest
+```
+
+```
+https://github.com/clhuang/heroku-buildpack-webp-binaries.git
+```
+
+```
+heroku/nodejs
+```
+
+## ✧✧✧ CMD Deployment Method
+
+```
+git clone https://github.com/Pelpav/Lelouch.git
+cd Lelouch
+npm i
+npm start
+```
+
+### ✧ Requis pour CMD istallation:
+
+- Git
+- Node.js
+- Libwebp
+- FFmpeg
+
+Note: Si vous ne pré-installez pas ces plugins avant l'installation de CMD, le bot ne peut pas exécuter les commandes de nom !
+</br>
+
+## ✧✧✧ Termux Deployment Method
+
+```js
+apt update
+apt upgrade
+pkg update && pkg upgrade
+pkg install bash
+pkg install libwebp
+pkg install git -y
+pkg install nodejs -y
+pkg install ffmpeg -y
+pkg install wget
+pkg install imagemagick -y
+git clone https://github.com/Pelpav/lelouch
+cd lelouch-MD
+npm i
+npm start
+```
+
+## ✧✧✧ Pour garder lelouch en vie sans internet 24h/24 et 7j/7
+
+```
+npm i -g pm2 && pm2 start index.js && pm2 save && pm2 logs
+```
+
+## ✧ Rappel :
+
+- Ce bot n'est pas créé par `WhatsApp Inc.` Donc, une mauvaise utilisation du bot pourrait `bannir` votre `compte WhatsApp !` (Bien que votre compte WhatsApp ne puisse être débanni qu'une seule fois.)
+- Je ne suis pas responsable du bannissement de votre compte.
+- Utilisez à vos risques et périls en gardant cet avertissement à l'esprit.
+
+## ✧ Aidez-moi :
+
+- [`Follow`](https://github.com/Pelpav/#follow) moi sur GitHub pour me motiver.
+- Donnez un ⭐ à mon projet si vous l'avez aimé.
---
+++ config.js
@@ -0,0 +1,133 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+global.available = true;
+global.autoReadGc = true;
+global.autoReadAll = false;
+global.antitags = true;
+
+global.Owner = ["22374429996"];
+global.OwnerNumber = ["22374429996"];
+global.ownertag = ["22374429996"];
+global.BotName = "Lelouch";
+global.packname = "Pelpav";
+global.author = "";
+global.OwnerName = "Pelpav";
+global.BotSourceCode = "https://github.com/Pelpav/Lelouch";
+global.SupportGroupLink = "https://chat.whatsapp.com/IroP01WmdScH5DSN9pY7C4";
+global.sessionName = "session";
+
+// Prefix //
+// If you want to change the prefix, change it and also change all switch's button id from Core.js  otherwise buttons will not work //
+global.prefa = ["-"];
+
+global.location = "West Bengal, India";
+global.reactmoji = "❤️";
+global.themeemoji = "💖";
+global.vidmenu = {
+  url: "https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4",
+};
+global.websitex = "https://github.com/FantoX001";
+global.lolhuman = "KaysaS";
+
+global.BotLogo = fs.readFileSync("./Assets/pic9.jpg");
+global.Thumb = fs.readFileSync("./Assets/pic2.jpg");
+global.Thumb1 = fs.readFileSync("./Assets/pic4.jpg");
+global.ErrorPic = fs.readFileSync("./Assets/pic6.jpg");
+
+global.ntilinkytvid = [];
+global.ntilinkytch = [];
+global.ntilinkig = [];
+global.ntilinkfb = [];
+global.ntilinktg = [];
+global.ntilinktt = [];
+global.ntilinktwt = [];
+global.ntilinkall = [];
+global.nticall = [];
+global.ntwame = [];
+global.nttoxic = [];
+global.ntnsfw = [];
+global.ntvirtex = [];
+global.rkyt = [];
+global.wlcm = [];
+global.gcrevoke = [];
+global.autorep = [];
+global.ntilink = [];
+
+/*
+global.mess = {
+    jobdone: 'Job done...',
+    useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
+    botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
+    botowner: 'Only my *Owner* can use this command, Baka!',
+    grouponly: 'This command is only made for *Groups*, Baka!',
+    privateonly: 'This command is only made for *Private Chat*, Baka!',
+    botonly: 'Only the *Bot itself* can use this command!',
+    waiting: 'Chotto Matte...',
+    nolink: 'Please provide me *link*, Baka!',
+    error: 'An error occurd!',
+    banned: 'You are *Banned* fron using commands!',
+    bangc: 'This Group is *Banned* from using Commands!',
+    nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+    
+}
+*/
+global.mess = {
+  jobdone: "Fait !...",
+  useradmin:
+    "Désolé, seuls les *administrateurs de groupe* peuvent utiliser cette commande **Baka** !",
+  botadmin:
+    "Désolé, je ne peux pas exécuter cette commande sans être *Admin* de ce groupe.",
+  botowner: "Seul mon *Propriétaire* peut utiliser cette commande, *Baka* !",
+  grouponly: "Cette commande n'est faite que pour les *Groupes*, *Baka* !",
+  privateonly: "Cette commande n'est faite que pour *Private Chat*, *Baka* !",
+  botonly: "Seul le *Bot lui-même* peut utiliser cette commande !",
+  waiting: "Chotto Matte...",
+  nolink: "Veuillez me fournir *lien*, *Baka* !",
+  error: "Une erreur s'est produite !",
+  banned: "Vous êtes *Banni* d'utiliser des commandes !",
+  bangc: "Ce groupe est *interdit* d'utiliser les commandes !",
+  nonsfw:
+    "Ne sois pas un pervers *Baka* ! Ce n'est pas un groupe activé NSFW !",
+};
+
+global.limitawal = {
+  premium: "Infinity",
+  free: 2,
+  monayawal: 1000,
+};
+global.rpg = {
+  darahawal: 100,
+  besiawal: 95,
+  goldawal: 30,
+  emeraldawal: 8,
+  umpanawal: 10,
+  potionawal: 5,
+};
+global.limitawal = {
+  rakyat: "Infinity",
+  free: 100,
+};
+
+global.APIs = {
+  zenz: "https://zenzapis.xyz",
+};
+global.APIKeys = {
+  "https://zenzapis.xyz": "5d1197db351b",
+};
+
+global.flaming =
+  "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=";
+global.fluming =
+  "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=";
+global.flarun =
+  "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=";
+global.flasmurf =
+  "https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text=";
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+  fs.unwatchFile(file);
+  console.log(chalk.redBright(`Update'${__filename}'`));
+  delete require.cache[file];
+  require(file);
+});
